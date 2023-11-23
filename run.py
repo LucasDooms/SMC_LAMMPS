7d0f9ba44300bb5c46fa65748136890c4aab4fec
Lucas Dooms <lucas.dooms@hotmail.com>
2023-11-23 15:10:07 +0100

    rewrite run.py using argparser

--- run.py
+++ run.py
@@ -1,65 +1,24 @@
+import argparse
+from pathlib import Path
 import subprocess
-import numpy as np
-from parse import readParameters
 
-runStart = 13
 
-# Number of independent runs
-nRuns = 12
+parser = argparse.ArgumentParser(
+    prog='Run LAMMPS',
+    description='runs the LAMMPS file and optionally runs python setup scripts',
+    epilog='End'
+)
 
-# Read parameters
-parameters = readParameters()
 
-datafile = 'initial.dat'
+parser.add_argument('directory', help='the directory containing parameters for LAMMPS')
+parser.add_argument('-g', '--generate', action='store_true', help='run the python setup scripts before executing LAMMPS')
+parser.add_argument('-e', '--executable', help='name of the LAMMPS executable to use', default="lmp")
+parser.add_argument('-i', '-in', '--input', help='path to input file to give to LAMMPS', default="input")
 
-subprocess.call(['python3', 'generate.py',
-                 '%s' %parameters['N'],
-                 '%s' %parameters['n'],
-                 '%s' %parameters['armLength'],
-                 '%s' %parameters['bridgeWidth'],
-                 '%s' %parameters['HKradius'],
-                 '%s' %parameters['epsilon3'],
-                 '%s' %parameters['epsilon6'],
-                 '%s' %parameters['cutoff6'],
-                 '%s' %parameters['intRadSMCvsDNA'],
-                 '%s' %parameters['armsStiffness'],
-                 '%s' %parameters['elbowsStiffness'],
-                 '%s' %parameters['siteStiffness'],
-                 '%s' %parameters['foldingAngleAPO'],
-                 '%s' %parameters['foldingStiffness'],
-                 '%s' %parameters['asymmetryStiffness'],
-                 datafile ])
+args = parser.parse_args()
+path = Path(args.directory)
 
-for run in range(runStart,nRuns+runStart):
+if args.generate:
+    subprocess.run(f"python generate+parse.py {path.absolute()}".split(" "))
 
-    outputfile = '%spN_run_%s.lammpstrj' %(str(parameters['forces']).replace('.','p'), run)
-
-    subprocess.Popen(['~/Documents/lammps-29Oct20/src/lmp_serial', '-in', 'input',
-                     '-var', 'N',                  '%s' %parameters['N'],
-                     '-var', 'force',              '%s' %parameters['forces'],
-                     '-var', 'armLength',          '%s' %parameters['armLength'],
-                     '-var', 'bridgeWidth',        '%s' %parameters['bridgeWidth'],
-                     '-var', 'epsilon3',           '%s' %parameters['epsilon3'],
-                     '-var', 'epsilon4',           '%s' %parameters['epsilon4'],
-                     '-var', 'epsilon5',           '%s' %parameters['epsilon5'],
-                     '-var', 'epsilon6',           '%s' %parameters['epsilon6'],
-                     '-var', 'cutoff4',            '%s' %parameters['cutoff4'],
-                     '-var', 'cutoff5',            '%s' %parameters['cutoff5'],
-                     '-var', 'cutoff6',            '%s' %parameters['cutoff6'],
-                     '-var', 'sigma',              '%s' %parameters['intRadSMCvsDNA'],
-                     '-var', 'armsStiffness',      '%s' %parameters['armsStiffness'],
-                     '-var', 'armsAngleATP',       '%s' %parameters['armsAngleATP'],
-                     '-var', 'foldingStiffness',   '%s' %parameters['foldingStiffness'],
-                     '-var', 'asymmetryStiffness', '%s' %parameters['asymmetryStiffness'],
-                     '-var', 'foldingAngleAPO',    '%s' %parameters['foldingAngleAPO'],
-                     '-var', 'foldingAngleATP',    '%s' %parameters['foldingAngleATP'],
-                     '-var', 'stepsATP',           '%s' %parameters['stepsATP'],
-                     '-var', 'stepsADP',           '%s' %parameters['stepsADP'],
-                     '-var', 'stepsAPO',           '%s' %parameters['stepsAPO'],
-                     '-var', 'datafile',           datafile,
-                     '-var', 'outputfile',         outputfile,
-                     '-var', 'seed',               '%s' %np.random.randint(1,100000),
-                     '-log',    'run_%s.log'    %run,
-                     '-screen', 'run_%s.screen' %run])
-
-    subprocess.call(['sleep', '2.0'])
+subprocess.run(f"{args.executable} -in {Path(args.input).absolute()}", cwd=path.absolute())
