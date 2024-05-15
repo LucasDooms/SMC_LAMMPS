e14d9b46b4613931ea7b26b0a5a88a936baf8d18
Lucas Dooms <lucas.dooms@hotmail.com>
2024-05-15 20:32:51 +0200

    Revert "setup isolated obstacle run"
    This reverts commit 3a76b4254f4f7cb8258e082a54cc58de6efd2f82.

--- 7nm_kleisin/parameters.py
+++ 7nm_kleisin/parameters.py
@@ -1,21 +1,21 @@
 # Radius of lower circular-arc compartment (nm)
-HKradius = 7.
+HKradius = 5.5
 # HKradius = 4.5 # from original paper (used incorrect formula??)
 
 # amount of DNA
 N = 600
 
-cycles = 20
+cycles = 75
 
 # bottom site
 # epsilon6 = 500.0
 
 # weak force
-force = None
+force = 0.03
 
 # configuration
-dnaConfig = "advanced_obstacle_safety"
-# dnaConfig = "obstacle_safety"
+# dnaConfig = "advanced_obstacle_safety"
+dnaConfig = "obstacle_safety"
 # dnaConfig = "obstacle"
 # dnaConfig = "line"
 
