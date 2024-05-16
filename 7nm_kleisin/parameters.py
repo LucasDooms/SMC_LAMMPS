68d3bc6788802c86ee6dd8eaaaa13a08e5751b32
Lucas Dooms <lucas.dooms@hotmail.com>
2024-05-16 16:42:19 +0200

    setup basic run

--- 7nm_kleisin/parameters.py
+++ 7nm_kleisin/parameters.py
@@ -5,18 +5,18 @@ HKradius = 5.5
 # amount of DNA
 N = 600
 
-cycles = 75
+cycles = 100
 
 # bottom site
 # epsilon6 = 500.0
 
 # weak force
-force = 0.03
+force = 0.07
 
 # configuration
 # dnaConfig = "advanced_obstacle_safety"
-dnaConfig = "obstacle_safety"
-# dnaConfig = "obstacle"
+# dnaConfig = "obstacle_safety"
+dnaConfig = "obstacle"
 # dnaConfig = "line"
 
 # smc_force = 0.01
