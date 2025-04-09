f6705cfa4dac17526962844abc083185b80f4a01
Lucas Dooms <lucas.dooms@hotmail.com>
2025-04-09 13:30:57 +0200

    add kleisin separately in vmd

--- post-process/visualize.py
+++ post-process/visualize.py
@@ -33,6 +33,7 @@ class Molecules:
 
     def __init__(self, path_to_vmd_init: Path) -> None:
         self.index = -1
+        self.rep_index = 0
         self.color_index = 0
         self.path = path_to_vmd_init / "vmd.init"
         # clear the file
@@ -45,29 +46,45 @@ class Molecules:
         return color_id
 
     def create_new(self, file_name: str, other_args: str) -> None:
-        with open(self.path, 'a') as file:
+        with open(self.path, 'a', encoding="utf-8") as file:
             file.write(f"mol new {file_name} {other_args}\n")
             self.index += 1
-    
+
     def create_new_marked(self, file_name: str) -> None:
         self.create_new(file_name, "waitfor all")
-        with open(self.path, 'a') as file:
+        with open(self.path, 'a', encoding="utf-8") as file:
             file.write(f"mol modstyle 0 {self.index} vdw\n")
-    
-    def create_new_dna(self, file_name: str, dna_pieces: List[Tuple[int, int]]) -> None:
+
+    def create_new_dna(self, file_name: str, dna_pieces: List[Tuple[int, int]], remove_ranges: List[Tuple[int, int]]) -> None:
         self.create_new(file_name, "waitfor all")
-        with open(self.path, 'a') as file:
+        with open(self.path, 'a', encoding="utf-8") as file:
             # show everything, slightly smaller
             file.write(f"mol modstyle 0 {self.index} cpk 0.9\n")
 
-            # color the pieces differently
-            file.write(f"mol rep cpk\n")
-            rep_index = 0
-            for piece in dna_pieces:
-                file.write(f"mol addrep {self.index}\n")
-                rep_index += 1
-                file.write(f"mol modselect {rep_index} {self.index} index >= {piece[0]} and index <= {piece[1]}\n")
-                file.write(f"mol modcolor {rep_index} {self.index} colorID {self.get_color_id()}\n")
+            # remove from ranges
+            selections = []
+            for rng in remove_ranges:
+                selections.append(f"index < {rng[0] - 1} or index > {rng[1] - 1}")
+            file.write(f"mol modselect 0 {self.index} " + " and ".join(selections) + "\n")
+
+            self.add_dna_pieces(file, dna_pieces)
+
+    def add_dna_pieces(self, file, dna_pieces: List[Tuple[int, int]]) -> None:
+        # color the pieces differently
+        file.write("mol rep cpk\n")
+        for piece in dna_pieces:
+            file.write(f"mol addrep {self.index}\n")
+            self.rep_index += 1
+            file.write(f"mol modselect {self.rep_index} {self.index} index >= {piece[0] - 1} and index <= {piece[1] - 1}\n")
+            file.write(f"mol modcolor {self.rep_index} {self.index} colorID {self.get_color_id()}\n")
+
+    def add_piece(self, rng: Tuple[int, int]) -> None:
+        with open(self.path, 'a', encoding="utf-8") as file:
+            file.write("mol rep cpk\n")
+            file.write(f"mol addrep {self.index}\n")
+            self.rep_index += 1
+            file.write(f"mol modselect {self.rep_index} {self.index} index >= {rng[0] - 1} and index <= {rng[1] - 1}\n")
+            file.write(f"mol modcolor {self.rep_index} {self.index} colorID {self.get_color_id()}\n")
 
 
 mol = Molecules(path)
@@ -75,7 +92,10 @@ mol = Molecules(path)
 for p in path.glob("marked_bead*.lammpstrj"):
     mol.create_new_marked(p.name)
 
-mol.create_new_dna("output.lammpstrj", parameters["dna_indices_list"])
+kleisins = parameters["kleisin_ids"]
+kleisin_rng = (min(kleisins), max(kleisins))
+mol.create_new_dna("output.lammpstrj", parameters["dna_indices_list"], [kleisin_rng])
+mol.add_piece(kleisin_rng)
 
 cmd = ["vmd", "-e", f"{mol.path.absolute()}"]
-subprocess.run(cmd, cwd=path)
+subprocess.run(cmd, cwd=path, check=True)
