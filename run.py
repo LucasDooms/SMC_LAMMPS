3f0647e4ec30865e7903add3ad55a92b4762eae5
Lucas Dooms <lucas.dooms@hotmail.com>
2025-04-09 15:49:34 +0200

    check for existing files before running
    add force parameter to run.py to disable checking

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ parser.add_argument('-s', '--seed', help='set the seed to be used by LAMMPS, thi
 parser.add_argument('-p', '--post-process', action='store_true', help='run the post-processing scripts after running LAMMPS')
 parser.add_argument('-n', '--ignore-errors', action='store_true', help='keep running even if the previous script exited with a non-zero error code')
 parser.add_argument('-v', '--visualize', action='store_true', help='open VMD after all scripts have finished')
+parser.add_argument('-f', '--force', action='store_true', help='don\'t prompt before overwriting existing files')
 parser.add_argument('-e', '--executable', help='name of the LAMMPS executable to use', default="lmp")
 parser.add_argument('-i', '-in', '--input', help='path to input file to give to LAMMPS', default="input.lmp")
 parser.add_argument('-o', '--output', help='path to dump LAMMPS output to (prints to terminal by default)')
@@ -41,28 +42,35 @@ if args.generate:
     if args.seed:
         extra_args.append(args.seed)
     print("running setup file...")
-    run_and_handle_error(lambda: subprocess.run(["python", "generate/generate+parse.py", f"{path}"] + extra_args), args.ignore_errors)
+    run_and_handle_error(lambda: subprocess.run(["python", "generate/generate+parse.py", f"{path}"] + extra_args, check=False), args.ignore_errors)
     print("succesfully ran setup file")
 
 
-run_with_output = partial(subprocess.run, [f"{args.executable}", "-in", f"{Path(args.input).absolute()}"], cwd=path.absolute())
+# check if output.lammpstrj exists
+file_exists = (path / "output.lammpstrj").exists()
 
-if args.output:
-    with open(args.output, 'w') as output_file:
-        print(f"running LAMMPS file {args.input}, output redirected to {args.output}")
-        run_and_handle_error(lambda: run_with_output(stdout=output_file), args.ignore_errors)
+if args.force or not file_exists:
+    run_with_output = partial(subprocess.run, [f"{args.executable}", "-in", f"{Path(args.input).absolute()}"], cwd=path.absolute())
+
+    if args.output:
+        with open(args.output, 'w', encoding="utf-8") as output_file:
+            print(f"running LAMMPS file {args.input}, output redirected to {args.output}")
+            run_and_handle_error(lambda: run_with_output(stdout=output_file), args.ignore_errors)
+    else:
+        print(f"running LAMMPS file {args.input}, printing output to terminal")
+        run_and_handle_error(run_with_output, args.ignore_errors)
 else:
-    print(f"running LAMMPS file {args.input}, printing output to terminal")
-    run_and_handle_error(lambda: run_with_output(), args.ignore_errors)
+    print("cannot run lammps script, output.lammpstrj already exists (use -f to overwrite files)")
+    print("moving on...")
 
 if args.post_process:
     print("running post processing...")
-    run_and_handle_error(lambda: subprocess.run(["python", "post-process/process_displacement.py", f"{path}"]), args.ignore_errors)
+    run_and_handle_error(lambda: subprocess.run(["python", "post-process/process_displacement.py", f"{path}"], check=False), args.ignore_errors)
     print("succesfully ran post processing")
 
 if args.visualize:
     print("starting VMD")
-    run_and_handle_error(lambda: subprocess.run(["python", "post-process/visualize.py", f"{path}"]), args.ignore_errors)
+    run_and_handle_error(lambda: subprocess.run(["python", "post-process/visualize.py", f"{path}"], check=False), args.ignore_errors)
     print("VMD exited")
 
 print("end of run.py")
