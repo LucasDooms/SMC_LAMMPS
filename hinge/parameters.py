6aa601e58f1dcad96f88f42582e20a8ba726fcc9
Lucas Dooms <lucas.dooms@hotmail.com>
2025-04-10 09:01:44 +0200

    update hinge parameters.py

--- hinge/parameters.py
+++ hinge/parameters.py
@@ -6,17 +6,7 @@ N = 350
 
 cycles = 20
 
-# Average number of steps for ATP binding
-stepsATP = 1000000
-
-# Average number of steps for ATP hydrolysis
-stepsADP = 4000000
-
-# Average number of steps for returning to APO
-stepsAPO = 1000000
-
-# bottom site
-# epsilon6 = 500.0
+rigidHinge = True
 
 # weak force
 force = 0.05
