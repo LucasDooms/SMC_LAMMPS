5cc1fa2be6eb8770c990853559c54d0910dcfb13
Lucas Dooms <lucas.dooms@hotmail.com>
2026-01-14 09:03:36 +0100

    sphinx: do not skip __init__ method by default

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,16 @@ linkcheck_ignore = [
 ]
 
 
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
+
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
